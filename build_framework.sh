b516b61eafeda08454280946b6473dcf01a047fb
Kalyan Vishnubhatla <kalvish@gmail.com>
2017-04-11 14:18:54 -0700

    Updated script variable name

--- build_framework.sh
+++ build_framework.sh
@@ -6,27 +6,27 @@ CONFIGURATION="Release"
 BUILD_ROOT="."
 PROJECT_NAME="SmyteSDK"
 TARGET_NAME=${PROJECT_NAME}
-UNIVERSAL_OUTPUTFOLDER="."
+OUTPUT_FOLDER="."
 
 # Step 1. Build Device and Simulator versions of the framework
 xcodebuild -target ${TARGET_NAME} ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 xcodebuild -target ${TARGET_NAME} -configuration ${CONFIGURATION} -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
 # Step 2. Make sure the output directory exists
-mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
-cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+mkdir -p "${OUTPUT_FOLDER}"
+cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" "${OUTPUT_FOLDER}/"
 
 # Step 3. Copy Swift modules from iphonesimulator build (if it exists) to the copied framework directory
 SIMULATOR_SWIFT_MODULES_DIR="${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule/."
 if [ -d "${SIMULATOR_SWIFT_MODULES_DIR}" ]; then
-cp -R "${SIMULATOR_SWIFT_MODULES_DIR}" "${UNIVERSAL_OUTPUTFOLDER}/${PROJECT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule"
+cp -R "${SIMULATOR_SWIFT_MODULES_DIR}" "${OUTPUT_FOLDER}/${PROJECT_NAME}.framework/Modules/${PROJECT_NAME}.swiftmodule"
 fi
 
 # Step 4. Create universal binary file using lipo and place the combined executable in the copied framework directory
-lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME}"
+lipo -create -output "${OUTPUT_FOLDER}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME}"
 
 # Step 5. Convenience step to copy the framework to the project's directory
-cp -R "${UNIVERSAL_OUTPUTFOLDER}/${PROJECT_NAME}.framework" "${PROJECT_DIR}"
+cp -R "${OUTPUT_FOLDER}/${PROJECT_NAME}.framework" "${PROJECT_DIR}"
 
 # Step 6. Open the output directory folder (for convenience)
-# open ${UNIVERSAL_OUTPUTFOLDER}
+# open ${OUTPUT_FOLDER}
